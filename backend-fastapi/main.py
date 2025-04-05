e6b85af186a626bd05745832546595e607b1184f
Shakthi Vel <mail2shakthivel98@gmail.com>
2025-04-05 22:53:26 +0200

    added sample env file

--- backend-fastapi/main.py
+++ backend-fastapi/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
 
 app = FastAPI()
+
+load_dotenv()
+
+TWITCH_CLIENT_ID = os.getenv("TWITCH_CLIENT_ID")
+TWITCH_CLIENT_SECRET = os.getenv("TWITCH_CLIENT_SECRET")
+
 
 @app.get("/")
 def home():
     return {"message": "Twitch Video Search Backend is running"}
+
+
+
+
