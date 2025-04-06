fa67df6378cf3448a30d067b615dd35678768a31
Shakthi Vel <mail2shakthivel98@gmail.com>
2025-04-06 21:06:09 +0200

    developed backend game search endpoint

--- backend-fastapi/main.py
+++ backend-fastapi/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 import os
 from dotenv import load_dotenv
+from services.twitch_api import search_videos_by_game
+
 
 app = FastAPI()
 
@@ -15,5 +17,7 @@ def home():
     return {"message": "Twitch Video Search Backend is running"}
 
 
-
+@app.get("/search")
+async def search(game: str):
+    return await search_videos_by_game(game)
 
